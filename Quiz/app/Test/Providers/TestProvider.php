8ff2c928088dd7da441a0940e5719d3a96b1865a
pbtcameron <pbtcameron@gmail.com>
2017-04-27 14:25:15 -0500

    Add comments

--- Quiz/app/Test/Providers/TestProvider.php
+++ Quiz/app/Test/Providers/TestProvider.php
@@ -1,7 +1,27 @@
 <?php
 namespace App\Test\Providers;
 
+/*
+    <Purpose>
+    A class to manage the test/quiz questions,
+    possible answers and the students responses to be stored in the 
+    Session
+    </Purpose>
+*/
+
 class TestProvider{
+    /*
+        <Purpose> 
+            Gets the answers and questions from the database
+            and sets them as class properties
+        </Purpose>
+    */
+    function __contruct(int $testId){
+        
+    }
+    //array of question classes
+    public $questions;
+    
     
 }
 ?>
