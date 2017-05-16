05b55c3a9c8be72a0d51d24f723c78aaddd62cb8
Ironbeetle <nomaduty@gmail.com>
2017-05-16 15:53:57 -0500

    Add files via upload
    only module builder and it should be ok, This is a laravel view

--- InstructorHome.blade.php
+++ InstructorHome.blade.php
@@ -1,115 +1,181 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
-    <meta name="description" content="">
-    <meta name="keywords" content="">
-    <title>Instructor home</title>
-	<link rel="stylesheet" href= "/css/styles.css">
-	<link rel="stylesheet" href="/css/bootstrap.min.css">
-	<link rel="stylesheet" href="/css/bootstrap.css">
-	<link rel="stylesheet" href="/css/bootstrap-grid.css">
-	<link rel="stylesheet" href="/css/bootstrap-grid.min.css">
+@extends('layout')
 
-	<script src="js/bootstrap.min.js"></script>
+@section ('content')
 
-    <!--[if IE]>
-    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-      <div class="container-fluid">
-          <div class="row">
-            <div class="col-md-12 banner">
-            <a href="#">Test Session</a>
-            <a href="#" style="float: right">Logout</a>
-              
+
+  <!---MENU PANEL START--->
+  <div class="row">
+    <div class="selectPanel">
+        <div class="col-md-3">
+            <input type="button" value="Create Modules" id="Modulebuilder"
+            onclick="javascript: modulebuilder();"/>
+        </div>
+        <div class="col-md-3">
+            <input type="button" value="Quiz Admin" id="Quizbuilder"
+            onclick="javascript: quizbuilder();" />
+        </div>
+        <div class="col-md-3">
+            <input type="button" value="View Quizes" id="QuizView"
+            onclick="javascript: quizviewshare();"/>
+        </div>
+        <div class="col-md-3">
+            <input type="button" value="Deploy Quiz" id="QuizDeploy"
+            onclick="javascript: quizdeploy();"/>
+        </div>
+    </div>
+  </div>
+  <!---MENU PANEL END--->
+  <div id="quizadminTABcontainer">
+    <div class="row">
+      <!---SCREEN LEFT START--->
+          <div class="col-md-6">
+              <div class="workarea">
+                <!---MODULE  SELECT QUIZ CREATE SECTION START--->
+                <div class="quizCreateStart" id="quizCreateStart">
+                  <h3>Build Quiz Template</h3>
+                <div class="QBsections3">
+                <!---BUILD NEW QUIZ START--->
+                <form method="POST" action="/newQuiz">
+                  <div class="QuizBuildStart">
+                      <div class="QuizBuildStartCell">
+                        <select name='ModuleID'>
+                          @foreach($modules as $m)
+                            <option value="{{$m->ModuleID}}"> {{$m->ModuleName}}</option>
+                          @endforeach
+                        </select>
+                      </div>
+                      <div class="QuizBuildStartCell">
+                        Quiz Name:
+                        <input type="text" name="QuizName">
+                      </div>
+                  </div>
+                  <div class="QuizBuildStart">
+                      <div class="QuizBuildStartCell">
+                          <button type="submit">Save Template</button>
+                      </div>
+                      <div class="QuizBuildStartCell">
+                        <button onclick="return hideNewQuiz()">Cancel</button>
+                        {!! csrf_field() !!}
+                      </div>
+                  </div>
+                </form>
+              <!---BUILD NEW QUIZ END--->
             </div>
-          </div>
-          <div class="row">
-              <div class="col-md-3">
-                <div class="row templateSelect">
-                  Select Builder <br/>
-                  <select text="Select Type" >
-                  <option> Select Builder</option>
-                  <option> Quiz Builder</Option>
-                  <option> Q & A Builder </option>
-                
+              <!---TEST VIEW PANEL START--->
 
-                  </select>
-                  </br>
-                  <input style="float: right" type="Button" value="Go To"/>
-                </div>
-                <div class="row quizSelect">
-                    <input type="Button" value="View"/>
-                </div>
-                <div class="row sendQuiz">
-                     <input type="Button" value="View"/>
-                </div>
-              </div>
-
-              <div class="col-md-6 workarea">
-                    <div id="QA" style="visibility: visible; background-color: cyan" >
-                <select id="ModulesSelect" >
-                <option>Select Module </option>
-                <option>Module #1 </option>  
-                <option>Module #2 </option>
-                <option>Module #3 </option>
-                </select>
-                <table>
-                <tr>
-                <td colspan="2" >Type Your Question Below</td>
-                </tr>
-                <tr>
-                <td></td>
-                <td ><textarea type="textarea" rows="5" cols="50" row="5" placeholder="Question" id="Question"></textarea><td>
-                </tr>
-                <tr>
-                <td>A)</td>
-                <td><input type="Text" placeholder="Option 1" id="Option1" /></td>
-                </tr>
-                <tr>
-                <td>B)</td>
-                <td><input type="Text" placeholder="Option 2" id="Option2" /></td>
-                </tr>
-                <tr>
-                <td>C)</td>
-                <td><input type="Text" placeholder="Option 3" id="Option3" /></td>
-                </tr>
-                <tr>
-                <td>D)</td>
-                <td><input type="Text" placeholder="Option 4" id="Option4" /></td>
-                </tr>
-                <tr>
-                <td>E)</td>
-                <td><input type="Text" placeholder="Option 5" id="Option5" /></td>
-                </tr>
-                <tr>
-                <td>F)</td>
-                <td><input type="Text" placeholder="Option 6" id="Option6" /></td>
-                </tr>
-                <tr ><td colspan="2" >Type the Letter of the right answer below</td></tr>
-                <tr>
-                <td></td>
-                <td ><input type="text" placeholder="Example: A" id="RightAnswer" /></td>
-                </tr>        
-                </table>
-
-                
-                                <input type="Button" value="Previous" style="float:left;"  id="Prev"/>
-                                                <input type="Button" value="Next" style="float:left;"  id="Next"/>
-                                <input type="Button" value="Finish" style="float:right;"  id="Finish"/>
-                    </div>
-
-              </div>
-
-              
-              <div class="col-md-3 workview">
-                <ul><li>Quiz #1</li></ul>
-                 <ul><li>Quiz #2</li></ul>
-              </div>
-          </div>
+              <!---TEST VIEW PANEL END--->
+        </div>
       </div>
-  </body>
-  </html>
+    </div>
+      <!---SCREEN LEFT END--->
+      <!---SCREEN RIGHT START--->
+          <div class="col-md-6">
+                <!---Quiz LIST VIEW START--->
+                <div class="QuizListcontainer" id="QuizListcontainer">
+                    @foreach($quizzes as $q)
+                      <div class="QuizListCell">
+                        <div class="QuizListCellName_Mod">
+                            <div class="QuizListCellSelect">
+                                <p>{{$q->ModuleName}}</p>
+                            </div>
+                            <div class="QuizListCellSelect">
+                                <p>{{$q->QuizName}}</p>
+                            </div>
+                        </div>
+                        <div class="QuizListCellControl">
+
+                            <div class="QuizDetail">
+                              <div class="QuizEdit">
+                                  <h5>Active:</h5>
+                              </div>
+                              <div class="QuizEditt">
+                                  <h5>{{$q->Active}}</h5>
+                              </div>
+                            </div>
+
+                            <div class="QuizDetail">
+                                <div class="QuizEdit">
+                                    <a href="/quiz/{{$q->QuizID}}"><h5>Edit</h5></a>
+                                </div>
+                                <div class="QuizEditt">
+                                    <a href="/quiz/{{$q->QuizID}}/delete"><h5>Delete</h5></a>
+                                </div>
+                            </div>
+
+                        </div>
+                      </div>
+                    @endforeach
+                </div>
+
+                <!---QUIZ LIST VIEW END--->
+          </div>
+      <!---SCREEN RIGHT END--->
+    </div>
+  </div>
+
+  <div id="moduleTABcontainer">
+    <div class="row">
+      <div class="col-md-6">
+        <!---MODULE BUILDER START--->
+                      <div class="modbuildcontainer">
+
+                          <div class="modbuildd">
+                            <div class="QMSelectors">
+                              <label for="modulename">Module Name: </label>
+                              <input type="text" id="moduleName"/>
+                            </div>
+                            <div class="QMSelectorss">
+                              <input type="button" value="Save Module" id="btnModuleEnter"/>
+                            </div>
+                          </div>
+                          <div class="modbuild">
+                            <div class="QMSelectors">
+                              <p>saved module name here</p>
+                            </div>
+                          </div>
+
+                      </div>
+                  <!---MODULE BUILDER ENDS--->
+      </div>
+      <div class="col-md-6">
+        <!---MODULE LIST VIEW START--->
+          <div id="ModuleListViewTABcontainer">
+            <!---MODULE LIST VIEW START--->
+              <div class="moduleListcontainer" id="moduleListcontainer">
+                @foreach($modules as $m)
+                <div class="ModuleListCell">
+                    <div class="ModuleListName" id="ModuleListSelect">
+                        <h3>{{$m->ModuleName}}</h3>
+                    </div>
+                </div>
+                @endforeach
+              </div>
+          </div>
+          <!---MOBILE LIST VIEW END--->
+      </div>
+    </div>
+  </div>
+
+  <div id="quizdeployTABcontainer">
+    <div class="row">
+        <h1>Quiz Deploy Section</h1>
+    </div>
+  </div>
+
+  <script type="text/javascript">
+
+  function showNewQuiz(){
+  divQuizzez.style.display = "none";
+  divNewQuiz.style.display = "block";
+  return false;
+  }
+
+  function hideNewQuiz(){
+  divQuizzez.style.display = "block";
+  divNewQuiz.style.display = "none";
+  return false;
+  }
+  </script>
+
+
+  @stop
