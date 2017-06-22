866ed4dde3089f5074e7336e8bff904081a2d804
pbtcameron <pbtcameron@gmail.com>
2017-06-22 17:46:24 -0500

    fixed time

--- Quiz/routes/web.php
+++ Quiz/routes/web.php
@@ -55,13 +55,7 @@ Route::get('/programByType','d3@getProgramsByType');
 Route::get('/charts',function(){ return view('chartTest');});
 Route::get('/donut',function(){return view('donut');});
 Route::get('/testOK','d3@getAllStudentMarksByQuiz');
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
-
 //function(){return "test";}
-
 
 
 
